97abbe4ef98f79b8e13284d98dd588e6f8035597
MatheusMBispo <matheus.filipe.bispo@concrete.com.br>
2020-02-19 18:00:51 -0300

    [Homebrew] New tap formulae

---
+++ secretsswift.rb
@@ -0,0 +1,31 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Secretsswift < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/MatheusMBispo/SecretsSwift/archive/0.1.0.tar.gz"
+  sha256 "f6bd75021a1447cea8fe0a7c94ec8100c8900d3928ff0f7e5778dc5e705e8dd6"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "make", "install","prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test SecretsSwift`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
